ac6011909e7c6077994a606172b4162edd812254
unknown <abdul.j@medlinkjobs.com>
2023-07-26 20:01:12 +0600

    done

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,8 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+//https://team-management-server.vercel.app
+
+// http://localhost:5000
+
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://team-management-server.vercel.app' }),
   tagTypes: ['authUser', 'getTeams'],
   endpoints: () => ({}),
 });
